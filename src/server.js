be6ed7b64f7d4a6a100e73c38063020f850e898c
Lucas Macedo Bernardino <lucasmacedoes@gmail.com>
2023-11-07 21:47:33 -0300

    test

--- src/server.js
+++ src/server.js
@@ -14,12 +14,6 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
 });
 
 
