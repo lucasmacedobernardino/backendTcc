770d26c09cde6f8f9aa1a3d2290a06011a1f8f8e
Lucas Macedo Bernardino <lucasmacedoes@gmail.com>
2023-11-07 21:28:03 -0300

    feat: add option cors

--- src/server.js
+++ src/server.js
@@ -4,18 +4,41 @@ import sequelize from "./config/database-connections.js";
 import routes from "./routes.js";
 import errorHandler from '../src/_middleware/error-handler.js';
 import authMiddleware from './_middleware/jwt-verify.js';
-const app = express();
+
 dotenv.config();
+const app = express();
+
+// Habilitar CORS
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Ajuste a origem específica ou lide com ela dinamicamente em vez de '*'
+    const allowedOrigins = ['http://localhost:3000', 'http://outrodominio.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+
+    // Responder imediatamente para requisições OPTIONS
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 
 app.use(express.json());
+
+// Middleware para verificação JWT deve vir antes das rotas
+app.use(authMiddleware);
+
 app.use(routes);
+
+// Middleware para manipulação de erros deve vir após as rotas
 app.use(errorHandler);
-app.use(authMiddleware)
-app.listen(3333);
+
+app.listen(process.env.PORT || 3333, () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT || 3333}`);
+});
