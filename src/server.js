8d69191faa13b4c560f7fa0c879a93c92cc0ff10
Lucas Macedo Bernardino <lucasmacedoes@gmail.com>
2023-11-13 20:12:53 -0300

    fix: allow cors for any people

--- src/server.js
+++ src/server.js
@@ -10,10 +10,12 @@ const app = express();
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization');
+    res.setHeader('Access-Control-Max-Age', '86400');
+    res.setHeader('Content-Type', "application/json")
 
     if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
+        res.sendStatus(204);
     } else {
         next();
     }
